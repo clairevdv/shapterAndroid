6a62f227279de78a7f0085fb31dbfc0dead09d50
clairevdv <claire_1992@hotmail.com>
2013-06-20 00:07:07 +0200

    Ajout d'une Action bar (pour l'instant dépendant de la hard key du menu)

--- gen/com/shapter/R.java
+++ gen/com/shapter/R.java
@@ -22,11 +22,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int pouf=0x7f020001;
+        public static final int ic_menu_ecole=0x7f020001;
+        public static final int ic_menu_eleves=0x7f020002;
+        public static final int ic_menu_login=0x7f020003;
+        public static final int ic_menu_overflow=0x7f020004;
+        public static final int ic_menu_ue=0x7f020005;
+        public static final int pouf=0x7f020006;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f080014;
-        public static final int action_settings=0x7f080013;
+        public static final int action_forgot_password=0x7f080019;
+        public static final int action_settings=0x7f080018;
         public static final int choixEcole=0x7f08000d;
         public static final int code=0x7f080011;
         public static final int container=0x7f080010;
@@ -40,6 +45,11 @@ public final class R {
         public static final int login_form=0x7f080005;
         public static final int login_status=0x7f080003;
         public static final int login_status_message=0x7f080004;
+        public static final int menu_ecole=0x7f080014;
+        public static final int menu_eleves=0x7f080015;
+        public static final int menu_login=0x7f080016;
+        public static final int menu_plus=0x7f080017;
+        public static final int menu_ue=0x7f080013;
         public static final int myFilter=0x7f08000e;
         public static final int password=0x7f080007;
         public static final int password_display=0x7f080002;
@@ -57,45 +67,51 @@ public final class R {
         public static final int liste_ue=0x7f030006;
     }
     public static final class menu {
-        public static final int all_products=0x7f070000;
-        public static final int connexion=0x7f070001;
-        public static final int description_ue=0x7f070002;
-        public static final int discovery=0x7f070003;
-        public static final int edit_product=0x7f070004;
-        public static final int loged=0x7f070005;
-        public static final int login=0x7f070006;
-        public static final int main=0x7f070007;
-        public static final int new_product=0x7f070008;
-        public static final int test=0x7f070009;
+        public static final int action_bar=0x7f070000;
+        public static final int all_products=0x7f070001;
+        public static final int connexion=0x7f070002;
+        public static final int description_ue=0x7f070003;
+        public static final int discovery=0x7f070004;
+        public static final int edit_product=0x7f070005;
+        public static final int loged=0x7f070006;
+        public static final int login=0x7f070007;
+        public static final int main=0x7f070008;
+        public static final int new_product=0x7f070009;
+        public static final int test=0x7f07000a;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f050013;
+        public static final int action_forgot_password=0x7f050018;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050011;
-        public static final int action_sign_in_short=0x7f050012;
+        public static final int action_sign_in_register=0x7f050016;
+        public static final int action_sign_in_short=0x7f050017;
         public static final int app_name=0x7f050000;
-        public static final int discover=0x7f050004;
-        public static final int error_field_required=0x7f050018;
-        public static final int error_incorrect_password=0x7f050017;
-        public static final int error_invalid_email=0x7f050015;
-        public static final int error_invalid_password=0x7f050016;
+        public static final int discover=0x7f050009;
+        public static final int error_field_required=0x7f05001d;
+        public static final int error_incorrect_password=0x7f05001c;
+        public static final int error_invalid_email=0x7f05001a;
+        public static final int error_invalid_password=0x7f05001b;
         public static final int hello_world=0x7f050002;
-        public static final int identification=0x7f050006;
-        public static final int login_progress_signing_in=0x7f050014;
-        public static final int mail=0x7f05000a;
-        public static final int mail_cible=0x7f05000b;
-        public static final int mdp=0x7f05000c;
-        public static final int mdp_cible=0x7f05000d;
+        public static final int identification=0x7f05000b;
+        public static final int login_progress_signing_in=0x7f050019;
+        public static final int mail=0x7f05000f;
+        public static final int mail_cible=0x7f050010;
+        public static final int mdp=0x7f050011;
+        public static final int mdp_cible=0x7f050012;
+        public static final int menu_ecole=0x7f050004;
+        public static final int menu_eleves=0x7f050005;
+        public static final int menu_login=0x7f050006;
+        public static final int menu_plus=0x7f050007;
+        public static final int menu_ue=0x7f050003;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f05000f;
-        public static final int prompt_password=0x7f050010;
-        public static final int recherche=0x7f050005;
-        public static final int title_activity_description_ue=0x7f050008;
-        public static final int title_activity_discovery=0x7f050007;
-        public static final int title_activity_loged=0x7f050009;
-        public static final int title_activity_login=0x7f05000e;
-        public static final int ue=0x7f050003;
+        public static final int prompt_email=0x7f050014;
+        public static final int prompt_password=0x7f050015;
+        public static final int recherche=0x7f05000a;
+        public static final int title_activity_description_ue=0x7f05000d;
+        public static final int title_activity_discovery=0x7f05000c;
+        public static final int title_activity_loged=0x7f05000e;
+        public static final int title_activity_login=0x7f050013;
+        public static final int ue=0x7f050008;
     }
     public static final class style {
         /** 
--- src/com/shapter/MainActivity.java
+++ src/com/shapter/MainActivity.java
@@ -3,6 +3,8 @@ package com.shapter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -22,5 +24,37 @@ public class MainActivity extends Activity {
 	    // Do something in response to button
 		Intent intentIdentify = new Intent(this, LoginActivity.class);
 		startActivity(intentIdentify);
+	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.action_bar, menu);
+        System.out.println("I'm here !");
+        return true;
+    }
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_ue:
+			Intent intentUE = new Intent(this, UEActivity.class);
+			startActivity(intentUE);
+			return true;
+		case R.id.menu_ecole:
+			// Comportement du bouton "Aide"
+			return true;
+		case R.id.menu_eleves:
+			// Comportement du bouton "Rafraichir"
+			return true;
+		case R.id.menu_login:
+			Intent intentLogin = new Intent(this, LoginActivity.class);
+			startActivity(intentLogin);
+			return true;
+		case R.id.menu_plus:
+			// Comportement du bouton "Paramètres"
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
